a5f7bfd93959df830b952aabd7d6bf2f6f963cad
XonqNopp <xonqnoppp@gmail.com>
2020-06-16 10:39:38 +0200

    [script/app] more but still not displaying

--- scripts/app.py
+++ scripts/app.py
@@ -15,29 +15,70 @@ class Recettes(QtWidgets.QMainWindow):
        * window title
        * post-actions
     """
-    def category(self):
-        self._category = QtWidgets.QButtonGroup()
-        self._cuisine = QtWidgets.QRadioButton('cuisine', self)
-        self._cosmetique = QtWidgets.QRadioButton('cosmétique', self)
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        layout = QtWidgets.QVBoxLayout()
+
+        category = self.categories()
+        layout.addWidget(category['cuisine'])
+        layout.addWidget(category['cosmetique'])
+
+        layout.addWidget(self.title())
+
+        template = self.templates()
+        layout.addWidget(template['default'])
+        layout.addWidget(template['personnes'])
+        layout.addWidget(template['plaques'])
+
+        button = self.buttons()
+        layout.addWidget(button['ok'])
+        layout.addWidget(button['cancel'])
+
+        self.setLayout(layout)
+
+    def categories(self):
+        self._categories = QtWidgets.QButtonGroup()
+
+        cuisine = QtWidgets.QRadioButton('cuisine', self)
+        cosmetique = QtWidgets.QRadioButton('cosmétique', self)
+
+        self._categories.addButton(cuisine)
+        self._categories.addButton(cosmetique)
+
+        return {'cuisine': cuisine, 'cosmetique': cosmetique}
 
     def title(self):
-        self._title = QtWidgets.QText()
-        self._title = QtWidgets.QLabel('Titre:', self)
-        self._title.setBuddy(QtWidgets.QLineEdit(self))
+        title = QtWidgets.QLabel('Titre:', self)
+        title.setBuddy(QtWidgets.QLineEdit(self))
+        return title
 
-    def template(self):
-        self._template = QtWidgets.QButtonGroup()
-        self._default = QtWidgets.QRadioButton('standard', self)
-        self._personnes = QtWidgets.QRadioButton('personnes', self)
-        self._plaques = QtWidgets.QRadioButton('petite et grande plaques', self)
+    def templates(self):
+        self._templates = QtWidgets.QButtonGroup()
+
+        default = QtWidgets.QRadioButton('standard', self)
+        personnes = QtWidgets.QRadioButton('personnes', self)
+        plaques = QtWidgets.QRadioButton('petite et grande plaques', self)
+
+        self._templates.addButton(default)
+        self._templates.addButton(personnes)
+        self._templates.addButton(plaques)
+
+        return {'default': default, 'personnes': personnes, 'plaques': plaques}
 
     def buttons(self):
         """
         OK cancel
         """
         self._buttons = QtWidgets.QButtonGroup()
-        self._ok = QtWidgets.QPushButton('OK', self)
-        self._cancel = QtWidgets.QPushButton('annuler', self)
+
+        ok = QtWidgets.QPushButton('OK', self)
+        cancel = QtWidgets.QPushButton('annuler', self)
+
+        self._buttons.addButton(ok)
+        self._buttons.addButton(cancel)
+
+        return {'ok': ok, 'cancel': cancel}
 
 
 def main():
