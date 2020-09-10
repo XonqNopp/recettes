0e5ce33b72bf576a57a2fa7bb58935e46f2cc049
XonqNopp <xonqnoppp@gmail.com>
2020-09-10 17:09:50 +0200

    [scripts] fix latex before typeset

--- scripts/view.sh
+++ scripts/view.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 # View the current state.
+
+# LaTeX PDF
 sphinx-build -W -b "latex" . "_build/latex" || exit 1
+python scripts/fix_latex.py || exit 1
 cd _build/latex && latexpawa Recettes.tex || exit 1
 
+# HTML
 sphinx-build -W -b "html" . "_build/html" || exit 1
 
